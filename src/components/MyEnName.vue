e328a7fe87a1672761b5b8f23f37cce106bead42
Sandeep Mehta <sandeepmehta08@users.noreply.github.com>
2026-04-12 18:24:47 +0530

    Fix Hero name overlap and adjust sizing

--- src/components/MyEnName.vue
+++ src/components/MyEnName.vue
@@ -1,16 +1,16 @@
 <template>
   <svg
     class="w-full fill-flax-smoke-900"
-    viewBox="0 0 450 95"
+    viewBox="0 0 900 120"
     xmlns="http://www.w3.org/2000/svg"
   >
     <text
       x="0"
-      y="85"
+      y="100"
       font-family="'Bricolage Grotesque', sans-serif"
       font-weight="900"
-      font-size="100"
-      letter-spacing="-4"
+      font-size="120"
+      letter-spacing="-2"
       class="translate-y-full will-change-auto"
     >
       SANDEEP
